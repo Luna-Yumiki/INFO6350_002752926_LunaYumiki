26482a50059c43a4fcda046ce66727c6c4a118bc
Luna <123215670+Luna-Yumiki@users.noreply.github.com>
2024-12-10 19:09:54 -0800

    Update main.dart

--- start/lib/main.dart
+++ start/lib/main.dart
@@ -1,3 +1,5 @@
+// Xianchu Wen was responsible for Steps 1 to 3, Luna took charge of Steps 4 to 6 in the final project.
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'app.dart';
